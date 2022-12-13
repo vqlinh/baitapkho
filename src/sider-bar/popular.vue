b3c10093af2cda5795fffff8d1f1b8f85672f835
unknown <vuongquanglinh1234@gmail.com>
2022-12-13 18:22:44 +0700

    succesfully

--- src/sider-bar/popular.vue
+++ src/sider-bar/popular.vue
@@ -1,13 +1,44 @@
 <template>
     <div class="popular">
         <div class="menu-top">
-            <span>POPULAR</span>
+            <span class="t1">POPULAR</span>
             <span>LATEST</span>
             <span>COMMENT</span>
             <span>TAGS</span>
             
         </div>
         <hr>
+        <div class="content">
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/happy-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">How to succeed with marketing content?</div>
+                    <div class="time">February 24,2016</div>
+                </div>
+            </div>
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/back-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">Smart Affiliate Marketing Strategies</div>
+                    <div class="time">February 24,2016</div>
+                </div>
+            </div>
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/start-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">Adsense Quick Start Guide</div>
+                    <div class="time">February 24,2016</div>
+                </div>
+            </div>
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/blogging-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">How to Start Making Money Blogging</div>
+                    <div class="time">February 24,2016</div>
+                </div>
+            </div>
+            
+        </div>
        
     </div>
   
@@ -23,12 +54,52 @@
     padding: 0;
     
 }
+.time{
+    opacity: 0.6;
+    cursor: pointer;
+    font-size: 14px;
+    margin-top: 8px;
+}
+
+.title{
+    margin-left: 20px;
+    width: 200px;
+}
+.big-title:hover{
+    color:#c51a30;
+    cursor: pointer;
+    
+
+}
+.content{
+    padding: 10px;
+}
+.ct1{
+    display: flex;
+    margin-left: 5px;
+    margin: 10px 0 10px 0;
+}
+
+img{
+    height: 64px;width: 64px;
+    cursor: pointer;
+}
 hr{
     width: 100%;
 }
-.menu-top{
+.menu-top{height: 35px;
     display: flex;
     justify-content: space-around;
-    padding: 0;
+    align-items: center;
+    padding: 10px 20px 10px 2px;
+    font-size: 14px;
+    font-weight: 550;
+    
+}
+.menu-top>*:hover{
+    color: #c51a30;
+}
+.t1{
+    color: #c51a30;
 }
 </style>>
--- src/sider-bar/viewed.vue
+++ src/sider-bar/viewed.vue
@@ -1,5 +1,32 @@
 <template>
-    <div class="viewed">
+    <div class="popular">
+        <div class="menu-top">
+            <span class="t1">MOST VIEWED</span>
+           
+            
+        </div>
+        <div class="content">
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/happy-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">How to succeed with marketing content?</div>
+                </div>
+            </div>
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/success-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">Smart Affiliate Marketing Strategies</div>
+                </div>
+            </div>
+            <div class="ct1">
+                <div class="image-1"><img src="https://demo.theme-junkie.com/beginner/files/2016/02/young-96x96.jpg" alt=""></div>
+                <div class="title">
+                    <div class="big-title">How to Use Social Media Marketing To Build Traffic</div>
+                </div>
+            </div>
+           
+            
+        </div>
        
     </div>
   
@@ -8,7 +35,60 @@
 <script>
 
 </script>
-
+                                
 <style scoped>
+*{
+    margin: 0;
+    padding: 0;
+    
+}
+.popular{
+    padding: 10px;
+}
+.time{
+    opacity: 0.6;
+    cursor: pointer;
+    font-size: 14px;
+    margin-top: 8px;
+}
 
+.title{
+    margin-left: 20px;
+    width: 200px;
+}
+.big-title:hover{
+    color:#c51a30;
+    cursor: pointer;
+    
+
+}
+.content{
+}
+.ct1{
+    display: flex;
+    margin-left: 5px;
+    margin-top: 10px;
+}
+
+img{
+    height: 64px;width: 64px;
+    cursor: pointer;
+}
+hr{
+    width: 100%;
+}
+.menu-top{height: 35px;
+    display: flex;
+    align-items: center;
+    padding: 10px 20px 10px 2px;
+    font-size: 14px;
+    font-weight: 550;
+    
+}
+.menu-top>*:hover{
+    color: #c51a30;
+}
+.t1{
+    color: #c51a30;
+}
 </style>>
